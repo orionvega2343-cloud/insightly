784bbb03602462fc9cd264575adcc7ccf8baef3a
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-28 22:22:12 +0300

    feat: add migrations

---
+++ migrations/000004_create_refresh_tokens.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS refresh_tokens(
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL REFERENCES users(id),
+    expires_at TIMESTAMP NOT NULL,
+    token TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+)
