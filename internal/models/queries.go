c9b2d202638922ee1d8732e184542f3a7fcdf5a2
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-29 18:15:04 +0300

    feat: add models

--- internal/models/queries.go
+++ internal/models/queries.go
@@ -1,1 +1,12 @@
 package models
+
+import "time"
+
+type Queries struct {
+	Id        int       `json:"id"`
+	UserId    int       `json:"user_id"`
+	FileId    int       `json:"file_id"`
+	CreatedAt time.Time `json:"created_at"`
+	Question  string    `json:"question"`
+	Answer    string    `json:"answer"`
+}
--- internal/models/users.go
+++ internal/models/users.go
@@ -1,1 +1,10 @@
 package models
+
+import "time"
+
+type User struct {
+	Id        int       `json:"id"`
+	Email     string    `json:"email"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
